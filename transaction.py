875c5b22904ecd5fe105b8da3098b23f9564ca1b
Kibnakamoto <kibnakanoto@protonmail.com>
2022-12-26 16:23:27 -0500

    fixes

--- transaction.py
+++ transaction.py
@@ -2,6 +2,7 @@
 # includes a list of transactions resetted every block
 
 from ecc, import sha512, ecc, curves
+import wallet
 
 # encode integer
 def encode_int(num, encoding='utf-8'):
@@ -16,9 +17,12 @@ def decode_int(encoded, encoding='utf-8'):
 class Pay2PubKeyHash:
     # r_pubkey is receivers public key
     def __init__(self, r_pubkey: tuple):
-        # public key hash to send to sender
-        self.pubkey_h = hkdf(r_pubkey[0], hashf=sha512.Sha512, hashlen=64, blocklen=128,
-                             outlen=32, keylen=66) # hash using hkdf
+        # public key hash to send to sender, encoded as base64
+        self.pubkey_h = wallet.gen_fingerprint(r_pubkey[0])
+        self.pubkey_h += wallet.gen_checksum(self.pubkey_h)
+        self.pubkey_h = base64.b64encode(self.pubkey_h).decode('utf-8').replace('=','')
+        #self.pubkey_h = hkdf(r_pubkey[0], hashf=sha512.Sha512, hashlen=64, blocklen=128,
+        #                     outlen=32, keylen=66) # hash using hkdf
 
         
 
