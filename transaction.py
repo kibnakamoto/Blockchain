6b712eb7d6e6c2d988ad638bf7b423a1f1d9f614
Kibnakamoto <kibnakanoto@protonmail.com>
2022-12-26 15:10:51 -0500

    transaction staerted

--- transaction.py
+++ transaction.py
@@ -1,1 +1,28 @@
+# transaction code for adding to mempool and data to send into the blocks
+# includes a list of transactions resetted every block
 
+from ecc, import sha512, ecc, curves
+
+# encode integer
+def encode_int(num, encoding='utf-8'):
+    return str(num).encode(encoding)
+
+# decode into integer
+def decode_int(encoded, encoding='utf-8'):
+    return int(encoded.decode(encoding))
+
+# Pay to Public Key Hash Structure
+# https://en.bitcoinwiki.org/wiki/Pay-to-Pubkey_Hash#Pay-to-PublicKey_Hash_Example
+class Pay2PubKeyHash:
+    # r_pubkey is receivers public key
+    def __init__(self, r_pubkey: tuple):
+        # public key hash to send to sender
+        self.pubkey_h = hkdf(r_pubkey[0], hashf=sha512.Sha512, hashlen=64, blocklen=128,
+                             outlen=32, keylen=66) # hash using hkdf
+
+        
+
+
+class Transaction:
+    def __init__(self):
+        pass
