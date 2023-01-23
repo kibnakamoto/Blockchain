4640d9f890c5587b50260d41d411d128535172ae
Kibnakamoto <kibnakanoto@protonmail.com>
2023-01-22 23:54:27 -0500

    debug

--- transaction.py
+++ transaction.py
@@ -19,45 +19,51 @@ class SignatureError(ValueError):
 
 # transaction data format to sign
 def tx_sign_data(prev_tx:str, block_index:int, b64_pub:bytes, amount: int, b64_pri:bytes):
-    return sha512.Sha512(sha512.Sha512(str(constants.VERSION).decode('hex') + prev_tx.encode('utf-8') +
-                                       int.to_bytes(block_index) + b64_pub + b64_pri +
-                                       int.to_bytes(amount)).digest()).digest() # message to sign
+    return sha512.Sha512(sha512.Sha512(int.to_bytes(constants.VERSION, 4, 'big') + prev_tx.encode('utf-8') +
+                                       str(block_index).encode('utf-8') + b64_pub + b64_pri +
+                                       str(amount).encode('utf-8')).digest()).digest().decode('charmap') # message to sign
 
 # transaction structure, used by the person who is sending
 class TxStructure:
     """ Default Class Initializer """
     def __init__(self, pubk:tuple, amount:float, block_index:int, ecdsa: ecc.Ecdsa, wlt: wallet.Wallet):
-        self.now = datetime.datetime.now()
+        self.now = str(datetime.datetime.now())
         self.amount = amount
         self.pubk = pubk # receiver
         self.block_index = block_index
-        if not os.stat("user/rtransactions.json").st_size != 0:
+        if os.path.getsize('user/rtransactions.json') != 0:
             with open('user/rtransactions.json','r') as file:
                 data = json.load(file)
-                self.prev_tx = data["transactions"][len(data)-1].keys()[0] # get previous transaction from user previous transactions
+                tx_count = len(data["transactions"])
+                print(data["transactions"])
+                self.prev_tx = data["transactions"][-1][str(tx_count-1)][-1]["hash"] # get previous transaction from user previous transactions
+                self.prev_block_i = data["transactions"][-1][str(tx_count-1)][-1]["block index"]
             # self.prev_tx = subprocess.check_output(['tail', '-1', 'prev_mempool']).remove('\n') # get previous transaction from mempool previous transactions
         else:
             self.prev_tx = "00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
+            self.prev_block_i = "0"
 
         # encode public key as base 64
         self.b64_pub = b""
-        self.b64_pub += base64.b64encode(int.to_bytes(self.pubk[0]))
+        self.b64_pub += base64.b64encode(str(self.pubk[0]).encode('utf-8'))
         self.b64_pub += b"|"
-        self.b64_pub += base64.b64encode(int.to_bytes(self.pubk[1]))
+        self.b64_pub += base64.b64encode(str(self.pubk[1]).encode('utf-8'))
 
         # encode private key as base 64
-        self.b64_pri = base64.b64encode(int.to_bytes(wlt.prikey))
+        self.b64_pri = base64.b64encode(str(wlt.prikey).encode('utf-8'))
         
         self.to_sign = tx_sign_data(self.prev_tx, self.block_index, self.b64_pub, self.amount, self.b64_pri) # message to sign
         self.signature = ecdsa.gen_signature(self.to_sign, wlt.prikey)
         self.m_hash = ecdsa.m_hash
         self.b64_signature = b""
-        self.b64_signature += base64.b64encode(int.to_bytes(self.signature[0]))
+        self.b64_signature += base64.b64encode(str(self.signature[1]).encode('utf-8'))
         self.b64_signature += b"|"
-        self.b64_signature += base64.b64encode(int.to_bytes(self.signature[1]))
+        self.b64_signature += base64.b64encode(str(self.signature[1]).encode('utf-8'))
 
-        self.info = str(constants.VERSION).decode('hex') + b" " + self.prev_tx.encode('utf-8') + b" " + self.b64_signature + b" " + \
-                    int.to_bytes(self.block_index) + b" " + int.to_bytes(self.amount) + b" " + self.b64_pub
+        self.info = int.to_bytes(constants.VERSION, 4, 'big') + b" " + self.prev_tx.encode('utf-8') + b" " + self.b64_signature + b" " + \
+                    str(self.block_index).encode('utf-8') + b" " + str(self.amount).encode('utf-8') + b" " + self.b64_pub
+        self.b64_pub = self.b64_pub.decode('utf-8')
+        self.b64_signature = self.b64_signature.decode('utf-8')
         self.tx_hash = sha512.Sha512(self.info).hexdigest()
 
 # wallet is senders wallet
@@ -65,6 +71,8 @@ class TxStructure:
 # amount of amount to send
 class Transaction(TxStructure):
     def __init__(self, wlt: wallet.Wallet, pubk: tuple, amount: float, block_index:int): # checksum is the first 4 bytes of the wallet
+        if amount <= 0.0:
+            raise AmountError("negative or non-existent amount")
         self.ecdsa = ecc.Ecdsa()
         self.ecies = ecc.Ecies()
         super().__init__(pubk, amount, block_index, self.ecdsa, wlt)
@@ -76,8 +84,8 @@ class Transaction(TxStructure):
     # call after ownership verified
     def add_transaction(self):
         if self.amount+self.fees <= self.wallet.balance:
-            mempool = open("mempool", "w")
-            mempool.write(self.tx_hash + self.now + "\n")
+            mempool = open("mempool", "a")
+            mempool.write(self.tx_hash + " " + self.now + " " + str(self.block_index) + " " + self.prev_tx + " " + str(self.prev_block_i) + " " + str(self.amount) + "\n")
             mempool.close()
             self.wallet.balance-=self.amount
             self.wallet.balance-=self.fees
@@ -88,26 +96,30 @@ class Transaction(TxStructure):
     def save(self):
         with open('user/prev_transactions.json','r+') as file:
             data = json.load(file)
-            id = len(data)-1 # id is the final values id, for order
+            id = len(data["transactions"])-1 # id is the final values id, for order
             data_add = {
+                "hash": self.tx_hash,
                 "time": self.now,
-                "private key": hex(self.wallet.prikey)[2:],
+                "private key": hex(self.wallet.prikey)[2:].zfill(8),
                 "version": hex(constants.VERSION)[2:],
                 "prev transaction": self.prev_tx,
                 "signature": self.b64_signature,
                 "to sign": self.to_sign,
                 "block index": hex(self.block_index)[2:],
                 "public key": self.b64_pub,
+                "amount": self.amount
             }
             # This condition will only check if transaction is isn't saved in the id-1.
             # This will prevent save being called twice without another transaction in between.
-            if id >= -1:
-                if self.tx_hash not in data["transactions"][id-1]: # add if tx_hash isn't already saved
-                    data["transactions"].append({id: {self.tx_hash: [data_add]}})
+            if id > 1:
+                # TODO this if statement will not be secure against hash that isn't the final value.
+                #      if id isn't len-1, it will pass through
+                if self.tx_hash not in data["transactions"][-1][str(id)][0]["hash"]: # add if tx_hash isn't already saved
+                    data["transactions"].append({str(id+1): [data_add]})
                     file.seek(0)
                     json.dump(data, file, indent=4)
             else: # if rtransactions is empty
-                data["transactions"].append({id: {self.tx_hash: [data_add]}})
+                data["transactions"].append({id+1: [data_add]})
                 file.seek(0)
                 json.dump(data, file, indent=4)
 
@@ -121,16 +133,14 @@ class Transaction(TxStructure):
 # tx_hash: transaction hash
 # return: hexdigest of new transaction hash
 def tx_receiver(wlt: wallet.Wallet, pubk: tuple, amount: float, block_index:int, tx_hash:str) -> str:
-    # TODO: to verify that the signature isn't made up. let sender share block index and timing and transaction hash of the sent hash to everyone so that
-    #       nodes can find check if transaction exists in the given block
     ecdsa = ecc.Ecdsa()
     # verified = ecdsa.verify_signature(signature, int.from_bytes(data), pubk)
 #    if verified:
     wlt.balance += amount
-    b64_prikey = base64.b64encode(int.to_bytes(wlt.prikey))
+    b64_prikey = base64.b64encode(str(wlt.prikey).encode('utf-8'))
     sign_data = tx_sign_data(tx_hash, block_index, wlt.b64(wlt.pubkey), amount, b64_prikey)
-    signature = ecdsa.gen_signature(sign_data, wlt.prikey) # genereate new signature for new ownership
-    with open('user/rtransactions.json','w') as file:
+    signature = ecdsa.gen_signature(sign_data, wlt.prikey) # generate new signature for new ownership
+    with open('user/rtransactions.json','w+') as file:
         data = json.load(file)
         b64_signature = b""
         b64_signature += base64.b64encode(int.to_bytes(signature[0]))
@@ -140,10 +150,11 @@ def tx_receiver(wlt: wallet.Wallet, pubk: tuple, amount: float, block_index:int,
         b64_pubk += base64.b64encode(int.to_bytes(pubk[0]))
         b64_pubk += b"|"
         b64_pubk += base64.b64encode(int.to_bytes(pubk[1])) # senders public key
-        new_hash_info = str(constants.VERSION).decode('hex') + b" " + tx_hash.encode('utf-8') + b" " + b64_signature + b" " + \
-                        int.to_bytes(block_index) + b" " + int.to_bytes(amount) + b" " + b64_pubk
+        new_hash_info = int.to_bytes(constants.VERSION, 4, 'big') + b" " + tx_hash.encode('utf-8') + b" " + b64_signature + b" " + \
+                        str(block_index).encode('utf-8') + b" " + str(amount).encode('utf-8') + b" " + b64_pubk
         new_hash = sha512.Sha512(new_hash_info).hexdigest()
         data_add = { # new transaction
+            "hash": new_hash,
             "time": str(datetime.datetime.now()),
             "private key": b64_prikey,
             "version": hex(constants.VERSION)[2:],
@@ -152,23 +163,32 @@ def tx_receiver(wlt: wallet.Wallet, pubk: tuple, amount: float, block_index:int,
             "to sign": sign_data,
             "block index": hex(block_index)[2:],
             "public key": b64_pubk,
+            "amount": amount
         }
         id = len(data) # id of transaction (for ordering rtransactions.json)
         if id != 0:
             id-=1
             # This condition will only check if transaction is isn't saved in the id-1.
             # This will prevent save being called twice without another transaction in between.
-            if new_hash not in data["transactions"][id-1]: # add if tx_hash isn't already saved
-                data["transactions"].append({id: {new_hash: [data_add]}})
+            if tx_hash not in data["transactions"][-1][str(id)][0]["hash"]: # add if tx_hash isn't already saved
+                data["transactions"].append({str(id): [data_add]})
                 file.seek(0)
                 json.dump(data, file, indent=4)
         else:
-            data["transactions"].append({0: {new_hash: [data_add]}})
+            data["transactions"].append({0: [data_add]})
             file.seek(0)
             json.dump(data, file, indent=4)
         return new_hash
 #    else:
 #        raise SignatureError("The signature isn\'t verified, false transaction, aborted")
+
+# If the amount you want to send is bigger than the previous transaction, recursively call tx_receiver for your own
+# transactions and combine their amounts. This way, your not creating money out of thin air and will be verified when mined.
+# If you don't use this method, it will not successfully mine and the amount will be locked
+# Basically sending money to yourself
+# Maybe in future versions, try implementing multiple previous hashes
+
+# In mining, verify if mempool transaction previous transaction exists in the specified block
 
 # alice and bob share their public keys and come up with their shared-secret
 w = curves.Weierstrass(constants.CURVE.p, constants.CURVE.a, constants.CURVE.b)
@@ -199,9 +219,13 @@ wlt1.new_keys()
 
 # verify checksum to make sure connection is secure
 verified_wallet_connection = wlt1.secure_com_receiver(b_shared_sec, ciphertext.decode('utf-8'), checksum)
-print(verified_wallet_connection)
 if verified_wallet_connection:
     # continue with transaction
     # Bob sends transaction to Alice
     wlt1.balance = 50
-    transaction = Transaction(wlt1, bob.pub_k, 1.0, 0)
+    transaction = Transaction(wlt1, wlt.pubkey, 5.0, 0)
+    transaction.add_transaction()
+    transaction.save()
+
+    # alice receives transaction
+    print(tx_receiver(wlt, wlt1.pubkey, 5.0, 0, transaction.tx_hash))
